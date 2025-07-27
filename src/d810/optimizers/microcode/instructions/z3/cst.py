a16b2083a41b5cb738934860efbc751af70ad05d
w00tzenheimer <w00tznheimer@nowhere.com>
2025-07-26 20:57:52 -0700

    Refactor Z3 constant optimization logic in cst.py 🚀
    - Converted validation logic to use short-circuiting, directly returning `None` if conditions are not met. This simplifies the flow and reduces nested logic. 🐍
    - Removed verbose logging for non-matching candidates and replaced it with conditional debug logging when debug mode is activated. This makes it cleaner and less noisy in production. 📉
    - Fixed potential ZeroDivisionError handling by ensuring error logs are clear and informative. Now they use `exc_info=True` for full traceback. 🛠️
    - Updated the way we handle leaf evaluation results to clarify conditions for optimization, eliminating ambiguity and potential errors in future code adjustments. 📏
    
    These changes ensure our optimization logic is safer, more efficient, and provides better debugging information! 🎉🎉

--- src/d810/optimizers/microcode/instructions/z3/cst.py
+++ src/d810/optimizers/microcode/instructions/z3/cst.py
@@ -4,14 +4,15 @@ import typing
 from ida_hexrays import *
 
 from d810 import _compat
+from d810.conf.loggers import LevelFlag
 from d810.errors import AstEvaluationException
 from d810.expr.ast import AstConstant, AstNode, minsn_to_ast
 from d810.expr.z3_utils import z3_check_mop_equality
 from d810.hexrays.hexrays_formatters import format_minsn_t
-from d810.hexrays.hexrays_helpers import equal_mops_bypass_xdu, get_mop_index
 from d810.optimizers.microcode.instructions.z3.handler import Z3Rule
 
 optimizer_logger = logging.getLogger("D810.optimizer")
+debug_on = LevelFlag(optimizer_logger.name, logging.DEBUG)
 
 
 class Z3ConstantOptimization(Z3Rule):
@@ -45,43 +46,55 @@ class Z3ConstantOptimization(Z3Rule):
         if tmp is None:
             return None
         leaf_info_list, cst_leaf_values, opcodes = tmp.get_information()
-        if (
-            len(leaf_info_list) == 1
-            and len(opcodes) >= self.min_nb_opcode
-            and (len(cst_leaf_values) >= self.min_nb_constant)
-        ):
-            optimizer_logger.info("Found candidate: %s", format_minsn_t(instruction))
-            try:
-                nb_leafs = len(leaf_info_list)
-                val_0 = tmp.evaluate_with_leaf_info(leaf_info_list, [0] * nb_leafs)
-                val_1 = tmp.evaluate_with_leaf_info(
-                    leaf_info_list, [0xFFFFFFFF] * nb_leafs
-                )
-                optimizer_logger.info("  val_0: %s, val_1: %s", val_0, val_1)
-                if val_0 == val_1 and tmp.mop is not None:
-                    optimizer_logger.info("  val_0 == val_1")
-                    c_res_mop = mop_t()
-                    c_res_mop.make_number(val_0, tmp.mop.size)
-                    is_ok = z3_check_mop_equality(tmp.mop, c_res_mop)
-                    optimizer_logger.info("  z3_check_mop_equality is equal? %s", is_ok)
-                    if is_ok:
-                        tmp.add_constant_leaf("c_res", val_0, tmp.mop.size)
-                        # Recompute caches so that leafs_by_name contains the new constant leaf
-                        tmp.compute_sub_ast()
+        leaf_num = len(leaf_info_list)
 
-                        new_instruction = self.get_replacement(
-                            typing.cast(AstNode, tmp)
-                        )
-                        return new_instruction
-                    return None
-                else:
-                    optimizer_logger.info("  val_0 != val_1")
-            except ZeroDivisionError:
-                pass
-            except AstEvaluationException as e:
-                print("Error while evaluating {0}: {1}".format(tmp, e))
-                pass
-        return None
+        if (
+            leaf_num != 1
+            or len(opcodes) < self.min_nb_opcode
+            or len(cst_leaf_values) < self.min_nb_constant
+        ):
+            return None
+
+        if debug_on:
+            optimizer_logger.debug("Found candidate: %s", format_minsn_t(instruction))
+        try:
+            val_0 = tmp.evaluate_with_leaf_info(leaf_info_list, [0])  # * leaf_num)
+            val_1 = tmp.evaluate_with_leaf_info(
+                leaf_info_list, [0xFFFFFFFF]
+            )  # * leaf_num)
+            if debug_on:
+                optimizer_logger.debug("  val_0: %s, val_1: %s", val_0, val_1)
+            if val_0 != val_1 or tmp.mop is None:
+                return None
+
+            # TODO(w00tzenheimer): if we're evaluating (evaluate_with_leaf_info) and the results are equal,
+            #   why do we need to run the z3 equality check?
+            #   why can't this simply be:
+            #   if val_0 != val_1 or tmp.mop is None:
+            #       return None
+            #   tmp.add_constant_leaf("c_res", val_0, tmp.mop.size)
+            #   tmp.compute_sub_ast()
+            #   new_instruction = self.get_replacement(typing.cast(AstNode, tmp))
+            #   return new_instruction
+            c_res_mop = mop_t()
+            c_res_mop.make_number(val_0, tmp.mop.size)
+            if z3_check_mop_equality(tmp.mop, c_res_mop):
+                if debug_on:
+                    optimizer_logger.debug("  z3_check_mop_equality is equal")
+
+                tmp.add_constant_leaf("c_res", val_0, tmp.mop.size)
+                # TODO(w00tzenheimer): should we recompute caches so that leafs_by_name contains the new constant leaf?
+                # tmp.compute_sub_ast()
+                new_instruction = self.get_replacement(typing.cast(AstNode, tmp))
+                return new_instruction
+        except ZeroDivisionError:
+            optimizer_logger.error(
+                "ZeroDivisionError while evaluating %s", tmp, exc_info=True
+            )
+        except AstEvaluationException as e:
+            optimizer_logger.error(
+                "Error while evaluating %s: %s", tmp, e, exc_info=True
+            )
 
     @_compat.override
     def check_candidate(self, candidate: AstNode) -> bool:
