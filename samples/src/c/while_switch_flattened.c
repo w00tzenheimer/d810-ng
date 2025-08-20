41b78b18fd19a8407d37e32c0bfed7b18a9a74d0
w00tzenheimer <w00tznheimer@nowhere.com>
2025-08-20 13:28:27 -0700

    feat(samples): Add build system and obfuscated C examples for testing
    - Introduced a new `Makefile` in the `samples` directory to facilitate building shared libraries across different platforms (Windows, macOS, Linux) with support for cross-targeting.
    - Added multiple obfuscated C source files (`manually_obfuscated.c`, `ollvm_obfuscated.c`, `tigress_obfuscated.c`, `unwrap_loops.c`, `while_switch_flattened.c`, and `constant_folding.c`) to provide examples for testing the plugin's functionality.
    - Updated `.gitignore` to include new build artifacts and temporary files generated during the build process.
    - Enhanced the `README.md` with instructions on how to build the sample binaries, including output naming conventions and make flags.

---
+++ samples/src/c/while_switch_flattened.c
@@ -0,0 +1,56 @@
+#include "polyfill.h"
+
+const uint64 qword_1802D2C33 = 0x736006A871C63D9ALL;
+
+// Does not fold ok
+uint64 while_switch_flattened(void)
+{
+    int v2 = 0;
+    int rval = 0x272BCB9A;
+    int v3 = 0;
+    int v4 = 0;
+    int v5 = 0;
+    uint64 a3 = 0;
+    uint64 globalConstant = __ROL8__(
+        (__ROL8__(
+             __ROL8__(0x736006A871C63D9ALL, 0x28) - 0x43401825757A7203LL,
+             0x2B) +
+         0x789F447F89C06931LL) ^
+            0x43B6AE2CD812A432LL,
+        0x2F);
+    while (1)
+    {
+        switch (v2)
+        {
+        case 0:
+            v3 = *(_QWORD *)NtCurrentTeb()->NtTib.FiberData;
+            v2 = 1;
+            break;
+
+        case 1:
+            v4 = __ROL8__(globalConstant, 0x11);
+            v2 = 2;
+            break;
+
+        case 2:
+            a3 = __ROL8__(
+                     (*(__int64 *)((char *)&qword_1802D2C33 + 3) ^ v4 ^ 0xCD455C5FB8140C43uLL) - 0x789F447F89C06931LL,
+                     0x15) +
+                 0x507AF58A6E5A51E3LL;
+            v2 = 3;
+            break;
+
+        case 3:
+            v5 = __ROL8__(a3 - 0xD3ADD64F8DFDFE0LL, 0x18);
+            v2 = 4;
+            break;
+
+        case 4:
+            rval = v3 == v5;
+            return rval;
+
+        default:
+            continue;
+        }
+    }
+}
