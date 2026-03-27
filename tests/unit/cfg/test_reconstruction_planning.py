8502028a84ce0e384e2cb85c39b949ad0354f9dd
w00tzenheimer <w00tznheimer@nowhere.com>
2026-03-27 16:20:35 -0700

    refactor(cfg): add reconstruction planning contract

---
+++ tests/unit/cfg/test_reconstruction_planning.py
@@ -0,0 +1,122 @@
+from __future__ import annotations
+
+from d810.cfg.reconstruction_planning import (
+    ReconstructionEmissionMode,
+    ReconstructionPlanningContext,
+    plan_reconstruction_candidate,
+)
+
+
+class _DummyBlock:
+    def __init__(self, preds: tuple[int, ...], succs: tuple[int, ...]):
+        self.preds = preds
+        self.succs = succs
+        self.npred = len(preds)
+        self.nsucc = len(succs)
+
+
+class _DummyFlowGraph:
+    def __init__(self, mapping: dict[int, tuple[tuple[int, ...], tuple[int, ...]]]):
+        self._mapping = {
+            int(k): _DummyBlock(tuple(int(v) for v in preds), tuple(int(v) for v in succs))
+            for k, (preds, succs) in mapping.items()
+        }
+
+    def get_block(self, serial: int):
+        return self._mapping.get(int(serial))
+
+
+class TestPlanReconstructionCandidate:
+    def test_accepts_direct_private_corridor(self):
+        flow_graph = _DummyFlowGraph({
+            14: ((12,), (16,)),
+            16: ((14,), (18,)),
+            18: ((16,), (20,)),
+        })
+        decision = plan_reconstruction_candidate(
+            flow_graph,
+            ReconstructionPlanningContext(
+                ordered_path=(14, 16, 18),
+                horizon_block=14,
+                target_entry=24,
+                source_anchor_block=10,
+                source_branch_arm=None,
+                is_conditional_transition=False,
+                shared_suffix_blocks=frozenset(),
+                dispatcher_region=frozenset(),
+                has_unsafe_trailing_insns=False,
+            ),
+        )
+        assert decision.accepted
+        assert decision.target_entry == 24
+        assert decision.emission_mode == ReconstructionEmissionMode.DIRECT
+        assert decision.first_shared_block is None
+
+    def test_accepts_conditional_arm_at_branch_horizon(self):
+        flow_graph = _DummyFlowGraph({
+            14: ((12,), (16, 18)),
+        })
+        decision = plan_reconstruction_candidate(
+            flow_graph,
+            ReconstructionPlanningContext(
+                ordered_path=(14, 16),
+                horizon_block=14,
+                target_entry=24,
+                source_anchor_block=14,
+                source_branch_arm=1,
+                is_conditional_transition=True,
+                shared_suffix_blocks=frozenset(),
+                dispatcher_region=frozenset(),
+                has_unsafe_trailing_insns=False,
+            ),
+        )
+        assert decision.accepted
+        assert decision.emission_mode == ReconstructionEmissionMode.CONDITIONAL_ARM
+
+    def test_falls_back_to_pred_split_for_shared_block(self):
+        flow_graph = _DummyFlowGraph({
+            12: ((8,), (14,)),
+            14: ((12, 13), (16,)),
+            16: ((14,), (18,)),
+        })
+        decision = plan_reconstruction_candidate(
+            flow_graph,
+            ReconstructionPlanningContext(
+                ordered_path=(12, 14, 16),
+                horizon_block=14,
+                target_entry=24,
+                source_anchor_block=10,
+                source_branch_arm=None,
+                is_conditional_transition=False,
+                shared_suffix_blocks=frozenset(),
+                dispatcher_region=frozenset(),
+                has_unsafe_trailing_insns=False,
+            ),
+        )
+        assert decision.accepted
+        assert decision.target_entry == 24
+        assert decision.emission_mode == ReconstructionEmissionMode.PRED_SPLIT
+        assert decision.first_shared_block == 14
+        assert decision.via_pred == 12
+
+    def test_rejects_when_no_shared_site_and_trailing_effects_exist(self):
+        flow_graph = _DummyFlowGraph({
+            12: ((8,), (16, 18)),
+        })
+        decision = plan_reconstruction_candidate(
+            flow_graph,
+            ReconstructionPlanningContext(
+                ordered_path=(12, 16),
+                horizon_block=12,
+                target_entry=24,
+                source_anchor_block=10,
+                source_branch_arm=None,
+                is_conditional_transition=False,
+                shared_suffix_blocks=frozenset(),
+                dispatcher_region=frozenset(),
+                has_unsafe_trailing_insns=True,
+            ),
+        )
+        assert not decision.accepted
+        assert decision.target_entry == 24
+        assert decision.rejection_reason == "blocked_side_effects"
