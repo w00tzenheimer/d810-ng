939bc39f72f1d0efeb39bafd7cb900c489c2ab89
w00tzenheimer <w00tznheimer@nowhere.com>
2026-05-31 19:19:59 -0700

    refactor(thinning): relocate handler_chain_topology_walk_backend -> d810.backends.hexrays.evidence.handler_chain_topology_walk_backend

--- tests/system/runtime/optimizers/microcode/flow/flattening/test_handler_chain_composer.py
+++ tests/system/runtime/optimizers/microcode/flow/flattening/test_handler_chain_composer.py
@@ -47,9 +47,7 @@ if IDA_AVAILABLE:
     )
     from d810.backends.hexrays.evidence import handler_chain_live_topology_backend as hcc_topology_backend_module
     from d810.backends.hexrays.evidence import handler_chain_materialization_capture_backend as hcc_capture_backend_module
-    from d810.optimizers.microcode.flow.flattening.hodur import (
-        handler_chain_topology_walk_backend as hcc_topology_walk_backend_module,
-    )
+    from d810.backends.hexrays.evidence import handler_chain_topology_walk_backend as hcc_topology_walk_backend_module
     from d810.analyses.control_flow.linearized_state_dag import (
         LinearizedStateDag,
         SemanticEdgeKind,
