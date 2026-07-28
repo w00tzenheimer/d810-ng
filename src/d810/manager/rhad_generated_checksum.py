b4c7f28a64418053d35d28b2377fa90ee6aa447e
w00tzenheimer <w00tzenheimer@users.noreply.github.com>
2026-07-28 07:42:52 -0700

    feat(rhad): compile row 85 indirect route

--- src/d810/manager/rhad_generated_checksum.py
+++ src/d810/manager/rhad_generated_checksum.py
@@ -4390,6 +4390,44 @@ _ROW84_EXISTING_ROUTE = RhadExistingConditionalRoute(
     depends_on=(_ROW83_CMOV_ROUTE.operation_id,),
 )
 
+_ROW85_CMOV_ROUTE = RhadConditionalRoute(
+    operation_id="rhad:route@0x40B17D",
+    reference_order=85,
+    operation_variant=RhadOperationVariant.CMOV_SELECTED_INDIRECT,
+    reference_symbol="JumpInliner._fixup_cmov",
+    source_block_id="native@0x40B163",
+    source_native_ea=0x40B16E,
+    source_block_anchor_ea=0x40B163,
+    transfer_ea=0x40B17D,
+    predicate_anchor_ea=0x40B176,
+    normalization_start_ea=0x40B176,
+    condition_producer_ea=0x40B168,
+    conditional_select_ea=0x40B176,
+    selected_value_block_id="native@0x40B176",
+    join_block_id="native@0x40B179",
+    observed_predicate_kind=PredicateKind.SGE,
+    predicate_kind=PredicateKind.SLT,
+    true_target_block_id="native@0x40B6C0",
+    false_target_block_id="native@0x40A607",
+    true_target_ea=0x40B6C0,
+    false_target_ea=0x40A607,
+    comparison_constant=0x0BB2D365,
+    owned_corridor_instruction_eas=(
+        0x40B168,
+        0x40B16E,
+        0x40B170,
+        0x40B176,
+        0x40B179,
+        0x40B17B,
+        0x40B17D,
+    ),
+    imported_closure_block_ids=ACCEPTED_IMPORTED_BLOCK_IDS,
+    boundary_exit_eas=(0x40A61B, 0x40A68C, 0x40B790),
+    flag_corridor_id="flags-intact@0x40B168",
+    phase=RhadReferencePhase.INDIRECT_JUMP_RECONSTRUCTION,
+    depends_on=(_ROW84_EXISTING_ROUTE.operation_id,),
+)
+
 _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
     batch_id="rhad-generated-reference@0x40A560",
     input_sha256=INPUT_SHA256,
@@ -4917,6 +4955,7 @@ _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
         _ROW82_EXISTING_ROUTE.operation_id,
         _ROW83_CMOV_ROUTE.operation_id,
         _ROW84_EXISTING_ROUTE.operation_id,
+        _ROW85_CMOV_ROUTE.operation_id,
     ),
     template_fragments=(
         RhadGeneratedTemplateFragment(
@@ -6553,6 +6592,7 @@ _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
         _ROW82_EXISTING_ROUTE,
         _ROW83_CMOV_ROUTE,
         _ROW84_EXISTING_ROUTE,
+        _ROW85_CMOV_ROUTE,
     ),
     required_boundary_exit_eas=BOUNDARY_EXIT_EAS,
     reference_commit="21b0d4783703bc4fb6910cfae51d92cd683d2c65",
