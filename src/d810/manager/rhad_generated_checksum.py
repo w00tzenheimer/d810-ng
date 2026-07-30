840b00d09ee2efb57f3bca0a28b803807d1a7142
w00tzenheimer <w00tzenheimer@users.noreply.github.com>
2026-07-30 09:40:29 -0700

    compiler: publish A560 row180 reference operation

--- src/d810/manager/rhad_generated_checksum.py
+++ src/d810/manager/rhad_generated_checksum.py
@@ -9389,6 +9389,30 @@ _ROW179_EXISTING_ROUTE = RhadExistingConditionalRoute(
     depends_on=(_ROW178_CMOV_ROUTE.operation_id,),
 )
 
+_ROW180_DIRECT_ROUTE = RhadDirectRoute(
+    operation_id="route:rhad-direct@0x40C14E",
+    reference_operation_id="rhad:route@0x40C14E",
+    reference_order=180,
+    operation_variant=RhadOperationVariant.SIMPLE_INDIRECT_JUMP,
+    reference_symbol="JumpInliner._fixup_jmp_and_possible_jcc",
+    source_block_id="native@0x40C144",
+    source_native_ea=0x40C132,
+    transfer_ea=0x40C14E,
+    owner_anchor_ea=0x40C144,
+    direct_target_block_id="native@0x40B6C0",
+    owned_corridor_instruction_eas=(
+        0x40C132,
+        0x40C144,
+        0x40C146,
+        0x40C148,
+        0x40C14E,
+    ),
+    imported_closure_block_ids=ROW19_TARGET_IMPORTED_BLOCK_IDS,
+    boundary_exit_eas=(0x40B790,),
+    phase=RhadReferencePhase.INDIRECT_JUMP_RECONSTRUCTION,
+    depends_on=(_ROW179_EXISTING_ROUTE.operation_id,),
+)
+
 _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
     batch_id="rhad-generated-reference@0x40A560",
     input_sha256=INPUT_SHA256,
@@ -10314,6 +10338,7 @@ _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
         _ROW177_EXISTING_ROUTE.operation_id,
         _ROW178_CMOV_ROUTE.operation_id,
         _ROW179_EXISTING_ROUTE.operation_id,
+        _ROW180_DIRECT_ROUTE.operation_id,
     ),
     template_fragments=(
         RhadGeneratedTemplateFragment(
@@ -13695,6 +13720,7 @@ _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
         _ROW177_EXISTING_ROUTE,
         _ROW178_CMOV_ROUTE,
         _ROW179_EXISTING_ROUTE,
+        _ROW180_DIRECT_ROUTE,
     ),
     required_boundary_exit_eas=BOUNDARY_EXIT_EAS,
     reference_commit="21b0d4783703bc4fb6910cfae51d92cd683d2c65",
