0d348597638ee342913cc03e488bbb5ec9cf7a8f
w00tzenheimer <w00tzenheimer@users.noreply.github.com>
2026-07-31 10:38:09 -0700

    rhad: compile row 225 direct route

--- src/d810/manager/rhad_generated_checksum.py
+++ src/d810/manager/rhad_generated_checksum.py
@@ -2385,6 +2385,13 @@ ROW224_TARGET_IMPORTED_BLOCK_IDS = (
     "native@0x40A680",
     "native@0x40A68A",
 )
+ROW225_TARGET_IMPORTED_BLOCK_IDS = (
+    "native@0x40A607",
+    "native@0x40A615",
+    "native@0x40A619",
+    "native@0x40A680",
+    "native@0x40A68A",
+)
 BASE_IMPORTED_LAYOUT = (
     (0x40A607, 0x40A615, (0x40A607,)),
     (0x40A615, 0x40A61B, (0x40A615,)),
@@ -11415,6 +11422,31 @@ _ROW224_DIRECT_ROUTE = RhadDirectRoute(
     depends_on=(_ROW223_DIRECT_ROUTE.operation_id,),
 )
 
+_ROW225_DIRECT_ROUTE = RhadDirectRoute(
+    operation_id="route:rhad-direct@0x40C840",
+    reference_operation_id="rhad:route@0x40C840",
+    reference_order=225,
+    operation_variant=RhadOperationVariant.SIMPLE_INDIRECT_JUMP,
+    reference_symbol="JumpInliner._fixup_jmp_and_possible_jcc",
+    source_block_id="native@0x40C83A",
+    source_native_ea=0x40C3BB,
+    transfer_ea=0x40C840,
+    owner_anchor_ea=0x40C83A,
+    direct_target_block_id="native@0x40A607",
+    owned_corridor_instruction_eas=(
+        0x40C3BB,
+        0x40C3CD,
+        0x40C83A,
+        0x40C83C,
+        0x40C83E,
+        0x40C840,
+    ),
+    imported_closure_block_ids=ROW225_TARGET_IMPORTED_BLOCK_IDS,
+    boundary_exit_eas=(0x40A61B, 0x40A68C),
+    phase=RhadReferencePhase.INDIRECT_JUMP_RECONSTRUCTION,
+    depends_on=(_ROW224_DIRECT_ROUTE.operation_id,),
+)
+
 _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
     batch_id="rhad-generated-reference@0x40A560",
     input_sha256=INPUT_SHA256,
@@ -12484,6 +12516,7 @@ _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
         _ROW222_DIRECT_ROUTE.operation_id,
         _ROW223_DIRECT_ROUTE.operation_id,
         _ROW224_DIRECT_ROUTE.operation_id,
+        _ROW225_DIRECT_ROUTE.operation_id,
     ),
     template_fragments=(
         RhadGeneratedTemplateFragment(
@@ -16294,6 +16327,7 @@ _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
         _ROW222_DIRECT_ROUTE,
         _ROW223_DIRECT_ROUTE,
         _ROW224_DIRECT_ROUTE,
+        _ROW225_DIRECT_ROUTE,
     ),
     required_boundary_exit_eas=BOUNDARY_EXIT_EAS,
     reference_commit="21b0d4783703bc4fb6910cfae51d92cd683d2c65",
