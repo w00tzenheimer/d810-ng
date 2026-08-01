3794fe33d99b9fb3c9a04e4b979c219cde567810
w00tzenheimer <w00tzenheimer@users.noreply.github.com>
2026-08-01 04:21:01 -0700

    rhad: compile row63 absolute constant

--- src/d810/manager/rhad_generated_checksum.py
+++ src/d810/manager/rhad_generated_checksum.py
@@ -13232,6 +13232,33 @@ _CONSTANT_ROW62_XOR_ABSOLUTE = RhadAbsoluteConstantMaterialization(
     depends_on=(_CONSTANT_ROW61_MOV_ABSOLUTE.operation_id,),
 )
 
+_CONSTANT_ROW63_MOV_ABSOLUTE = RhadAbsoluteConstantMaterialization(
+    operation_id="constant:rhad-mov-absolute@0x40C4F8",
+    reference_operation_id="rhad:constant@0x40C4F8",
+    reference_order=63,
+    operation_variant=RhadOperationVariant.MOV_ABSOLUTE,
+    reference_symbol="deob_consts.ConstantInliner.transform_mov_mem_to_imm",
+    source_block_id="native@0x40C4F6",
+    source_native_ea=0x40C4F8,
+    data_native_ea=0x48ADE8,
+    source_width_bits=32,
+    destination_width_bits=32,
+    destination_storage=StorageIdentity(
+        kind=StorageIdentityKind.REGISTER,
+        offset=12,
+    ),
+    reference_read_width_bits=32,
+    reference_data_bytes_le="727ce43a",
+    reference_raw_value=0x3AE47C72,
+    materialized_value=0x3AE47C72,
+    source_instruction_bytes="8b15e8ad4800",
+    replacement_instruction_bytes="90ba727ce43a",
+    encoding_variant=RhadAbsoluteConstantEncoding.MOV_R32_ABSOLUTE,
+    publication_envelope=RhadConstantPublicationEnvelope.IMPORTED_GLOBAL_MOVE,
+    phase=RhadReferencePhase.CONSTANT_MATERIALIZATION,
+    depends_on=(_CONSTANT_ROW62_XOR_ABSOLUTE.operation_id,),
+)
+
 _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
     batch_id="rhad-generated-reference@0x40A560",
     input_sha256=INPUT_SHA256,
@@ -18199,6 +18226,7 @@ _A560_GENERATED_REFERENCE_BATCH = RhadGeneratedReferenceBatch(
         _CONSTANT_ROW60_MOV_ABSOLUTE,
         _CONSTANT_ROW61_MOV_ABSOLUTE,
         _CONSTANT_ROW62_XOR_ABSOLUTE,
+        _CONSTANT_ROW63_MOV_ABSOLUTE,
     ),
     required_boundary_exit_eas=BOUNDARY_EXIT_EAS,
     reference_commit="21b0d4783703bc4fb6910cfae51d92cd683d2c65",
